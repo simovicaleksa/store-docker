cc021152adeee6e42332b64e7b6a5e2356fabfd8
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-04-26 19:11:18 +0200

    update prettier config

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,6 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: false,
+  singleQuote: false,
   plugins: ["prettier-plugin-tailwindcss"],
-};
+}
 
-export default config;
+export default config
