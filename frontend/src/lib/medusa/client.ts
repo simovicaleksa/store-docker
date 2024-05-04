2595dfdb4739dde336a241992767c373a6437e6c
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-04 21:14:22 +0200

    change client maxretries

--- frontend/src/lib/medusa/client.ts
+++ frontend/src/lib/medusa/client.ts
@@ -2,7 +2,7 @@ import Medusa from "@medusajs/medusa-js"
 
 const medusa = new Medusa({
   baseUrl: "https://srv520376.hstgr.cloud:9000",
-  maxRetries: 5,
+  maxRetries: 3,
 })
 
 export default medusa
