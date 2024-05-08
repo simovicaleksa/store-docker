0d57aa15dbe8a4d6b827077106a316898f3fc624
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-08 13:38:38 +0200

    allow custom class on product grid component

--- frontend/src/components/collections/ProductGrid.tsx
+++ frontend/src/components/collections/ProductGrid.tsx
@@ -5,18 +5,26 @@ import React from "react"
 import ProductCard from "../product/ProductCard"
 import { getCheapestProductPrice } from "@/utils/product"
 import useCurrencyCode from "@/hooks/countries/useCurrencyCode"
+import { cn } from "@/lib/utils"
 
 export default function ProductGrid({
   products,
+  className,
 }: {
   products: PricedProduct[] | undefined | null
+  className?: string
 }) {
   const { currencyCode } = useCurrencyCode()
 
   if (!products) return null
 
   return (
-    <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div
+      className={cn(
+        "grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4",
+        className,
+      )}
+    >
       {products.map((product) => (
         <ProductCard
           key={product.id}
