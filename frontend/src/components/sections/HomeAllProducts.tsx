7e0d0378057b6ac488b1d629fd9d192db884e842
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-09 20:40:40 +0200

    update layout for mobile

--- frontend/src/components/sections/HomeAllProducts.tsx
+++ frontend/src/components/sections/HomeAllProducts.tsx
@@ -3,6 +3,7 @@ import { Button } from "../shared/ui/button"
 import LocalizedClientLink from "../link/LocalizedClientLink"
 import { type PricedProduct } from "@medusajs/medusa/dist/types/pricing"
 import HomeAllProductsImage from "./HomeAllProductsImage"
+import { plusPattern } from "@/constants/patterns"
 
 export default function HomeAllProducts({
   products,
@@ -10,8 +11,11 @@ export default function HomeAllProducts({
   products: PricedProduct[] | undefined | null
 }) {
   return (
-    <div className="mx-auto my-20 flex min-h-[300px] max-w-7xl flex-col space-y-5 p-5 lg:flex-row">
-      <div className="flex w-full flex-col justify-center text-start">
+    <div
+      style={{ backgroundImage: plusPattern }}
+      className="bg- mx-auto my-20 flex min-h-[300px] max-w-7xl flex-col space-y-5 p-5 lg:flex-row"
+    >
+      <div className="flex w-full flex-col items-center justify-center text-center lg:items-start lg:text-start">
         <h2 className="text-3xl font-bold">View All Products</h2>
         <p className="text-muted-foreground">{`Didn't find what you were looking for?`}</p>
         <Button className="mt-5 w-fit" asChild>
--- frontend/src/components/sections/HomeAllProductsImage.tsx
+++ frontend/src/components/sections/HomeAllProductsImage.tsx
@@ -20,11 +20,11 @@ export default function HomeAllProductsImage({
     .slice(0, 3)
 
   return (
-    <ul className="hidden h-[600px] w-full flex-col items-center justify-center lg:flex">
+    <ul className="flex h-[600px] w-full flex-col items-center justify-center">
       {splicedProducts.map((product) => (
         <li
           key={product.id}
-          className="absolute first:z-10 first:-translate-y-48 first:translate-x-10 first:rotate-3 last:z-30 last:translate-y-48 last:-rotate-6 even:z-20 even:translate-x-32"
+          className="absolute mt-20 first:z-10 first:-translate-x-12 first:-translate-y-40 first:-rotate-6 last:z-30 last:translate-y-40 last:rotate-[9deg] even:z-20 even:translate-x-16 sm:mt-32 sm:first:-translate-x-32 sm:even:translate-x-32 sm:even:rotate-3 lg:mt-0 lg:first:-translate-y-48 lg:first:translate-x-10 lg:first:rotate-3 lg:last:translate-y-48 lg:last:-rotate-6 lg:even:translate-x-32"
         >
           <ProductCard
             collection={product.collection?.title}
@@ -32,7 +32,7 @@ export default function HomeAllProductsImage({
             handle={product.handle}
             price={getCheapestProductPrice(product, currencyCode)}
             thumbnail={product.thumbnail}
-            className="h-fit max-w-64"
+            className="h-fit max-w-48 sm:max-w-64"
           />
         </li>
       ))}
