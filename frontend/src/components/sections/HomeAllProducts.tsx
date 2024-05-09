d21efccf8bc06c830ad4eba74a08481eddfd14d9
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-09 20:41:57 +0200

    remove pattern style from smaller container

--- frontend/src/components/sections/HomeAllProducts.tsx
+++ frontend/src/components/sections/HomeAllProducts.tsx
@@ -3,7 +3,6 @@ import { Button } from "../shared/ui/button"
 import LocalizedClientLink from "../link/LocalizedClientLink"
 import { type PricedProduct } from "@medusajs/medusa/dist/types/pricing"
 import HomeAllProductsImage from "./HomeAllProductsImage"
-import { plusPattern } from "@/constants/patterns"
 
 export default function HomeAllProducts({
   products,
@@ -11,10 +10,7 @@ export default function HomeAllProducts({
   products: PricedProduct[] | undefined | null
 }) {
   return (
-    <div
-      style={{ backgroundImage: plusPattern }}
-      className="bg- mx-auto my-20 flex min-h-[300px] max-w-7xl flex-col space-y-5 p-5 lg:flex-row"
-    >
+    <div className="bg- mx-auto my-20 flex min-h-[300px] max-w-7xl flex-col space-y-5 p-5 lg:flex-row">
       <div className="flex w-full flex-col items-center justify-center text-center lg:items-start lg:text-start">
         <h2 className="text-3xl font-bold">View All Products</h2>
         <p className="text-muted-foreground">{`Didn't find what you were looking for?`}</p>
