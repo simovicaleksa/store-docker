fcc148736c978889383c652b74ba7f8b004613e5
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-21 16:45:01 +0200

    feat(store): auto hide header on scroll down

--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react"
 import SettingsDropdown from "@/components/settings/SettingsDropdown"
 import { Button } from "@/components/shared/ui/button"
@@ -5,10 +7,22 @@ import Link from "next/link"
 import CartTrigger from "@/components/cart/CartTrigger"
 import HeaderNavigation from "./HeaderNavigation"
 import { storeName } from "@/constants/store"
+import useScrollY from "@/hooks/shared/useScrollY"
+import { cn } from "@/lib/utils"
 
 export default function Header() {
+  const { scrollY, scrollDirection } = useScrollY()
+
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background p-5 text-secondary-foreground">
+    <header
+      className={cn(
+        "sticky top-0 z-50 w-full border-b bg-background p-5 text-secondary-foreground duration-200",
+        {
+          "py-2": scrollY !== 0,
+          "-translate-y-20": scrollDirection === "down" && scrollY > 500,
+        },
+      )}
+    >
       <div className="mx-auto flex w-full max-w-7xl flex-row items-center justify-between">
         <Button className="text-lg font-semibold" variant={"ghost"} asChild>
           <Link href="/">{storeName}</Link>
