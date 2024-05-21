2c0f7f5f6673a78e90d9150b00fb1a86d4a04f2a
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-21 16:44:50 +0200

    feat(store): add useScrollY hook

---
+++ frontend/src/hooks/shared/useScrollY.ts
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function useScrollY() {
+  const [scrollY, setScrollY] = useState<number>(0)
+  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("up")
+  const isScrolled = scrollY !== 0
+
+  useEffect(() => {
+    function handleScroll() {
+      const currentScroll = Math.floor(window.scrollY)
+
+      currentScroll > scrollY
+        ? setScrollDirection("down")
+        : setScrollDirection("up")
+
+      setScrollY(currentScroll)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [scrollY])
+
+  return { scrollY, isScrolled, scrollDirection }
+}
