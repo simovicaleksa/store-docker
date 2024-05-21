a1e94da367b9f0ff2b2da46ed50d17362a36654c
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-21 14:41:28 +0200

    style(store): make hero section more powerful

--- frontend/src/components/sections/HomeHeroBanner.tsx
+++ frontend/src/components/sections/HomeHeroBanner.tsx
@@ -13,13 +13,15 @@ export default function HomeHeroBanner({
     <div className="mx-auto flex min-h-[400px] w-full max-w-7xl items-center p-5 py-10">
       <div className="flex h-full w-full flex-col items-center space-y-14 lg:flex-row">
         <div className="flex w-full flex-col">
-          <div>
-            <h1 className="text-4xl font-bold">Shopping is better with us!</h1>
-            <p className="text-muted-foreground">
+          <div className="space-y-2">
+            <h1 className="text-4xl font-bold lg:text-5xl">
+              Shopping is better with us!
+            </h1>
+            <p className="font-semibold text-muted-foreground lg:text-xl">
               Buying things online was never easier.
             </p>
           </div>
-          <Button className="mt-5 w-fit" asChild>
+          <Button size={"lg"} className="mt-5 w-fit" asChild>
             <LocalizedClientLink href="/search">
               View Products
             </LocalizedClientLink>
