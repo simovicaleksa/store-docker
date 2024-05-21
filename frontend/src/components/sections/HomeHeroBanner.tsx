eda87659d9025552ecc384c62e5abe1851243907
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-21 16:15:39 +0200

    feat(store): dynamic hero button size

--- frontend/src/components/sections/HomeHeroBanner.tsx
+++ frontend/src/components/sections/HomeHeroBanner.tsx
@@ -1,14 +1,21 @@
+"use client"
+
 import React from "react"
 import { Button } from "../shared/ui/button"
 import LocalizedClientLink from "../link/LocalizedClientLink"
 import { type PricedProduct } from "@medusajs/medusa/dist/types/pricing"
 import HomeProductCard from "./HomeProductCard"
+import useTailwindBreakpoint from "@/hooks/shared/useTailwindBreakpoint"
 
 export default function HomeHeroBanner({
   product,
 }: {
   product: PricedProduct | undefined | null
 }) {
+  const { isBreakpoint } = useTailwindBreakpoint("sm")
+
+  const buttonSize = !isBreakpoint ? "default" : "lg"
+
   return (
     <div className="mx-auto flex min-h-[400px] w-full max-w-7xl items-center p-5 py-10">
       <div className="flex h-full w-full flex-col items-center space-y-14 lg:flex-row">
@@ -21,11 +28,23 @@ export default function HomeHeroBanner({
               Buying things online was never easier.
             </p>
           </div>
-          <Button size={"lg"} className="mt-5 w-fit" asChild>
-            <LocalizedClientLink href="/search">
-              View Products
-            </LocalizedClientLink>
-          </Button>
+          <div className="mt-5 flex flex-row flex-wrap gap-2">
+            <Button size={buttonSize} className="w-fit" asChild>
+              <LocalizedClientLink href="/search">
+                View Products
+              </LocalizedClientLink>
+            </Button>
+            <Button
+              size={buttonSize}
+              variant={"outline"}
+              className="w-fit"
+              asChild
+            >
+              <LocalizedClientLink href="/collections">
+                View Collections
+              </LocalizedClientLink>
+            </Button>
+          </div>
         </div>
         <HomeProductCard product={product} />
       </div>
