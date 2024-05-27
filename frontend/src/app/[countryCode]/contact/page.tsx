9b6b9ff2db6fd091341f90b168da646dda090955
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-28 00:24:34 +0200

    feat(store): add contact page

--- frontend/src/app/[countryCode]/contact/page.tsx
+++ frontend/src/app/[countryCode]/contact/page.tsx
@@ -1,5 +1,10 @@
+import ContactForm from "@/components/sections/ContactForm"
 import React from "react"
 
 export default function ContactPage() {
-  return <main className="min-h-screen"></main>
+  return (
+    <main className="flex min-h-screen w-full flex-row">
+      <ContactForm className="w-full max-w-2xl" />
+    </main>
+  )
 }
