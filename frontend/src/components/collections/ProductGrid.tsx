0ad49bfe9bad56569890ea4b22468f7fc2bdfab2
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-05-28 00:25:39 +0200

    feat(store): implement list view into product view

--- frontend/src/components/collections/ProductGrid.tsx
+++ frontend/src/components/collections/ProductGrid.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import { type PricedProduct } from "@medusajs/medusa/dist/types/pricing"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ProductCard from "../product/ProductCard"
 import { getCheapestProductPrice } from "@/utils/product"
 import useCurrencyCode from "@/hooks/countries/useCurrencyCode"
 import { cn } from "@/lib/utils"
+import { useSearchParams } from "next/navigation"
+import ProductListCard from "../product/ProductListCard"
 
 export default function ProductGrid({
   products,
@@ -14,9 +16,39 @@ export default function ProductGrid({
   products: PricedProduct[] | undefined | null
   className?: string
 }) {
+  const [viewType, setViewType] = useState<"grid" | "list">("grid")
+  const searchParams = useSearchParams()
   const { currencyCode } = useCurrencyCode()
 
+  useEffect(() => {
+    if (searchParams.has("view")) {
+      const view = searchParams.get("view")
+      if (typeof view === "string") {
+        if (view === "grid" || view === "list") {
+          setViewType(view)
+        }
+      }
+    }
+  }, [searchParams])
+
   if (!products) return null
+
+  if (viewType === "list") {
+    return (
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+        {products.map((product) => (
+          <ProductListCard
+            key={product.id}
+            title={product.title}
+            thumbnail={product.thumbnail}
+            collection={product.collection?.title}
+            handle={product.handle}
+            price={getCheapestProductPrice(product, currencyCode)}
+          />
+        ))}
+      </div>
+    )
+  }
 
   return (
     <div
