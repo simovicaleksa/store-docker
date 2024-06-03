bf59a7345700ad00272898a84eea2874027ad402
Aleksa1312 <102186502+Aleksa1312@users.noreply.github.com>
2024-06-03 16:41:04 +0200

    fix(store): font initialization

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -10,7 +10,20 @@ import { Inter } from "next/font/google"
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 })
+
+// const poppins = Poppins({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+//   weight: ["300", "400", "500", "600", "700", "800", "900"],
+// })
+
+// const rubik = Rubik({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+//   weight: ["300", "400", "500", "600", "700", "800", "900"],
+// })
 
 export const metadata = {
   title: `${storeName} | Better Shopping`,
@@ -24,7 +37,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body className={`${inter.className}`}>
         <JotaiProvider>
           <Overlay />
           <Header />
