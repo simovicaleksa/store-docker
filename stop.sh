87866c7f1616ffdef9a25b98729cf0fa9b7aff14
aleksa_simovic <102186502+AleksaSimovic1@users.noreply.github.com>
2024-12-14 15:12:59 +0100

    fix: make stop.sh script give option to stop dev/prod

--- stop.sh
+++ stop.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-docker compose down
+# Function to run Docker Compose
+stop_compose() {
+    local file=$1
+    echo "Running Docker Compose using $file..."
+    docker-compose --env-file ./.env -f $file down -d
+}
+
+# User prompt to select environment
+echo "Select environment: [dev/prod]"
+read ENVIRONMENT
+
+# Determine the compose file based on user input
+if [ "$ENVIRONMENT" == "prod" ]; then
+    COMPOSE_FILE="./compose.prod.yml"
+elif [ "$ENVIRONMENT" == "dev" ]; then
+    COMPOSE_FILE="./compose.dev.yml"
+else
+    echo "Invalid environment selected. Please choose 'dev' or 'prod'."
+    exit 1
+fi
+
+# Run the appropriate Docker Compose file
+run_compose $COMPOSE_FILE
